c38582f1bed17e515b0d44dda21c0d767f1b5f63
dinesh69ne-debug <dinesh69.ne@gmail.com>
2026-03-30 12:46:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -231,7 +231,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "dist")));
 
 // React router fix
-app.get("/*", (req, res) => {
+app.get("/.*/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
